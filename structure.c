5ac23592dfdee4fc229ce6ff8d6404fe3e9b77d1
sunkilaxmanraj <84630424+sunkilaxmanraj@users.noreply.github.com>
2021-06-03 20:37:59 +0530

    Add files via upload

--- structure.c
+++ structure.c
@@ -5,26 +5,32 @@ struct employee
 {
     char name[20];
     int id;
+    int experience;
     float salary;
 };
 int main()
 {
+
     int i=0;
-    struct employee s;
+    struct employee s[5];
     for(i=0;i<=4;i++)
     {
-        printf("enter name%d:",i+1);
-        gets(s.name);
+        //printf("enter details of employee %d \n",i+1);
+        printf("\nenter name %d:",i+1);
+        scanf("%s",&s[i].name);
         printf("enter id%d:",i+1);
-        scanf("%d",&s.id);
+        scanf("%d",&s[i].id);
+        printf("enter experience%d:",i+1);
+        scanf("%d",&s[i].experience);
         printf("enter the salary%d:",i+1);
-        scanf("%f",&s.salary);
+        scanf("%f",&s[i].salary);
     }
     for(i=0;i<=4;i++)
     {
-        printf("\nname:%s",i+1,s.name);
-        printf("\nid:%d",i+1,s.id);
-        printf("\nsalary:%f",i+1,s.salary);
+        printf("\n\n name:%s",s[i].name);
+        printf("\n id:%d",s[i].id);
+        printf("\n experience:%d",s[i].experience);
+        printf("\n salary:%f",s[i].salary);
     }
     return 0;
 }
